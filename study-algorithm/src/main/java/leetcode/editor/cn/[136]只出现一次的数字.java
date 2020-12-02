389d7e0e7b6707221e15e5742f473c882096b3a1
lixiaoshuang <644968328@qq.com>
2020-12-02 23:54:48 +0800

    只出现一次的数字

---
+++ study-algorithm/src/main/java/leetcode/editor/cn/[136]只出现一次的数字.java
@@ -0,0 +1,45 @@
+package leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+//
+// 说明： 
+//
+// 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？ 
+//
+// 示例 1: 
+//
+// 输入: [2,2,1]
+//输出: 1
+// 
+//
+// 示例 2: 
+//
+// 输入: [4,1,2,1,2]
+//输出: 4 
+// Related Topics 位运算 哈希表 
+// 👍 1602 👎 0
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public static int singleNumber(int[] nums) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> integerIntegerEntry : countMap.entrySet()) {
+            if (integerIntegerEntry.getValue() == 1) {
+                return integerIntegerEntry.getKey();
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {4, 1, 2, 1, 2};
+        int i = singleNumber(a);
+        System.out.println(i);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
