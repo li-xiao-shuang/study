d0e9e28c99c69f487725b5e970a58dd9c4c886d7
lixiaoshuang <644968328@qq.com>
2020-12-21 20:54:45 +0800

    lock

---
+++ study-concurrency/src/main/java/lock/ReentrantLockDemo.java
@@ -0,0 +1,25 @@
+
+package lock;
+
+public class ReentrantLockDemo {
+
+    public static void main(String[] args) {
+        final Count count = new Count();
+
+        for (int i = 0; i < 2; i++) {
+            new Thread() {
+                public void run() {
+                    count.get();
+                }
+            }.start();
+        }
+
+        for (int i = 0; i < 2; i++) {
+            new Thread() {
+                public void run() {
+                    count.put();
+                }
+            }.start();
+        }
+    }
+}
