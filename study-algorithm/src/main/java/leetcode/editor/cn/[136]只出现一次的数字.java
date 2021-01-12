a34cd1c9f345034398d8364e27c16fb2fa50494c
lixiaoshuang <644968328@qq.com>
2021-01-12 09:24:58 +0800

    树遍历

--- study-algorithm/src/main/java/leetcode/editor/cn/[136]只出现一次的数字.java
+++ study-algorithm/src/main/java/leetcode/editor/cn/[136]只出现一次的数字.java
@@ -1,38 +1,38 @@
-package leetcode.editor.cn;
-
-//给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+//package leetcode.editor.cn;
 //
-// 说明： 
+////给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+////
+//// 说明：
+////
+//// 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+////
+//// 示例 1:
+////
+//// 输入: [2,2,1]
+////输出: 1
+////
+////
+//// 示例 2:
+////
+//// 输入: [4,1,2,1,2]
+////输出: 4
+//// Related Topics 位运算 哈希表
+//// 👍 1602 👎 0
+////leetcode submit region begin(Prohibit modification and deletion)
+//class Solution {
+//    public static int singleNumber(int[] nums) {
+//        int result = 0;
+//        for (int num : nums) {
+//            result = result^num;
+//            System.out.println(result);
+//        }
+//        return result;
+//    }
 //
-// 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？ 
-//
-// 示例 1: 
-//
-// 输入: [2,2,1]
-//输出: 1
-// 
-//
-// 示例 2: 
-//
-// 输入: [4,1,2,1,2]
-//输出: 4 
-// Related Topics 位运算 哈希表 
-// 👍 1602 👎 0
-//leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public static int singleNumber(int[] nums) {
-        int result = 0;
-        for (int num : nums) {
-            result = result^num;
-            System.out.println(result);
-        }
-        return result;
-    }
-
-    public static void main(String[] args) {
-        int[] nums = {4, 1, 2, 1, 2};
-        int result = singleNumber(nums);
-        System.out.println(result);
-    }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+//    public static void main(String[] args) {
+//        int[] nums = {4, 1, 2, 1, 2};
+//        int result = singleNumber(nums);
+//        System.out.println(result);
+//    }
+//}
+////leetcode submit region end(Prohibit modification and deletion)
---
+++ study-algorithm/src/main/java/tree/TreeTest.java
@@ -0,0 +1,86 @@
+package tree;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class TreeTest {
+
+
+    public static TreeNode createBinaryTree(LinkedList<Integer> inputList) {
+        TreeNode node = null;
+        if (inputList == null || inputList.isEmpty()) {
+            return null;
+        }
+        Integer data = inputList.removeFirst();
+        if (data != null) {
+            node = new TreeNode(data);
+            node.leftChild = createBinaryTree(inputList);
+            node.rightChild = createBinaryTree(inputList);
+        }
+        return node;
+    }
+
+    /**
+     * 前序遍历
+     *
+     * @param node
+     */
+    public static void preOrderTraversal(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(node.data);
+        preOrderTraversal(node.leftChild);
+        preOrderTraversal(node.rightChild);
+    }
+
+    /**
+     * 中序遍历
+     *
+     * @param node
+     */
+    public static void inOrderTraversal(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        inOrderTraversal(node.leftChild);
+        System.out.println(node.data);
+        inOrderTraversal(node.rightChild);
+    }
+
+    /**
+     * 后序遍历
+     *
+     * @param node
+     */
+    public static void postOrderTraversal(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        postOrderTraversal(node.leftChild);
+        postOrderTraversal(node.rightChild);
+        System.out.println(node.data);
+    }
+
+
+    private static class TreeNode {
+        int data;
+        TreeNode leftChild;
+        TreeNode rightChild;
+
+        public TreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> inputList = new LinkedList<>(Arrays.asList(3, 2, 9, null, null, 10, null, null, 8, null, 4));
+        TreeNode binaryTree = createBinaryTree(inputList);
+        System.out.println("前序遍历");
+        preOrderTraversal(binaryTree);
+        System.out.println("中序遍历");
+        inOrderTraversal(binaryTree);
+        System.out.println("后序遍历");
+        postOrderTraversal(binaryTree);
+    }
+}
